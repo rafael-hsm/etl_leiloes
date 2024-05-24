cc825fca8d19e005a3907486387931a6dbcb7b97
rafael-hsm <rafameireles2011@gmail.com>
2024-05-24 11:46:39 -0300

    Alteração

--- src/etl_leiloes_veicular/settings.py
+++ src/etl_leiloes_veicular/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "etl_leiloes_veicular.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
+# USER_AGENT = "Informe seu USER AGENT AQUI"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
